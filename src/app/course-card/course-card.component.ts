875959fe8ab48aeee956ab0924dc36fb2f4c9994
PraveenKumar Natarajan <praveenkumar.natarajan@flatironssolutions.com>
2020-05-02 20:41:44 +0530

    9. Component Inputs and the @Input Decorator -> Adding Input(1)

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'course-card',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseCardComponent implements OnInit {
 
+  @Input()
+  title:string;
   constructor() { }
 
   ngOnInit(): void {
